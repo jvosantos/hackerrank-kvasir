4bed46d27afc7b8979eaaa8441c1df84f8172bb0
Vasco Santos <vasco.santos@mindera.com>
2016-07-25 08:44:32 +0100

    Add BinaryNumbers solution

---
+++ src/main/java/com/jvosantos/hackerrank/tutorials/thirtydaysofcode/BinaryNumbers.java
@@ -0,0 +1,28 @@
+package com.jvosantos.hackerrank.tutorials.thirtydaysofcode;
+
+public class BinaryNumbers {
+
+    public static int getMaximumConsecutiveOnes(int n) {
+        String binaryString = Integer.toBinaryString(n);
+
+        int maxSum = 0;
+        int currentSum = 0;
+
+        for(int i = 0; i < binaryString.length(); i++) {
+            if(binaryString.charAt(i) == '1') {
+                currentSum++;
+            } else {
+                if(currentSum > maxSum) {
+                    maxSum = currentSum;
+                }
+                currentSum = 0;
+            }
+        }
+
+        if(currentSum > maxSum) {
+            maxSum = currentSum;
+        }
+
+        return maxSum;
+    }
+}
---
+++ src/test/java/com/jvosantos/hackerrank/tutorials/thirtydaysofcode/BinaryNumbersTest.java
@@ -0,0 +1,36 @@
+package com.jvosantos.hackerrank.tutorials.thirtydaysofcode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BinaryNumbersTest {
+    @Test
+    public void testMaxConsecutiveOnesWithAllZeros() {
+        Assert.assertEquals(0, BinaryNumbers.getMaximumConsecutiveOnes(Integer.parseInt("000000", 2)));
+    }
+
+    @Test
+    public void testMaxConsecutiveOnesWithAllOnes() {
+        Assert.assertEquals(6, BinaryNumbers.getMaximumConsecutiveOnes(Integer.parseInt("111111", 2)));
+    }
+
+    @Test
+    public void testMaxConsecutiveOnesWithMaxInMiddle() {
+        Assert.assertEquals(2, BinaryNumbers.getMaximumConsecutiveOnes(Integer.parseInt("101101", 2)));
+    }
+
+    @Test
+    public void testMaxConsecutiveOnesStrictlyDecreasing() {
+        Assert.assertEquals(3, BinaryNumbers.getMaximumConsecutiveOnes(Integer.parseInt("111011", 2)));
+    }
+
+    @Test
+    public void testMaxConsecutiveOnesStrictlyIncreasing() {
+        Assert.assertEquals(3, BinaryNumbers.getMaximumConsecutiveOnes(Integer.parseInt("110111", 2)));
+    }
+
+    @Test
+    public void testMaxConsecutiveOnesWithAlternatingPattern() {
+        Assert.assertEquals(1, BinaryNumbers.getMaximumConsecutiveOnes(Integer.parseInt("101010", 2)));
+    }
+}
