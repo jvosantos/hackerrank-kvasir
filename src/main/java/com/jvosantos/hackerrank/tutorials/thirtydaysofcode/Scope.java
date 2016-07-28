8f21a23c5e2f6f20c2775bd0307ff90fc0678870
Vasco Santos <vasco.santos@mindera.com>
2016-07-28 10:39:15 +0100

    Added Scope solution

---
+++ src/main/java/com/jvosantos/hackerrank/tutorials/thirtydaysofcode/Scope.java
@@ -0,0 +1,37 @@
+package com.jvosantos.hackerrank.tutorials.thirtydaysofcode;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Scope {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sc.close();
+
+        Difference difference = new Difference(a);
+
+        difference.computeDifference();
+
+        System.out.print(difference.maximumDifference);
+    }
+}
+
+class Difference {
+    private int[] elements;
+    public int maximumDifference;
+
+    Difference(int[] elements) {
+        this.elements = elements;
+    }
+
+    void computeDifference() {
+        maximumDifference = Math.abs(IntStream.range(0, elements.length).map(i -> elements[i]).min().getAsInt()
+                - IntStream.range(0, elements.length).map(i -> elements[i]).max().getAsInt());
+    }
+}
